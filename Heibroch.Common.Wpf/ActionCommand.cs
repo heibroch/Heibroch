2a283db178d8a3992eb089911ec168e13359222f
Dwayne Heibroch Petersen <dwayne@live.dk>
2016-02-14 14:27:08 +0100

    Added command

---
+++ Heibroch.Common.Wpf/ActionCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Heibroch.Common.Wpf
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> action;
+
+        public ActionCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
