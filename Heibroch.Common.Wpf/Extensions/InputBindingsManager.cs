e4fc7e773b20347606146383041433cfeec1e95f
Dwayne Heibroch Petersen <dwayne@live.dk>
2016-04-27 09:55:13 +0200

    Added files.

---
+++ Heibroch.Common.Wpf/Extensions/InputBindingsManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Heibroch.Common.Wpf.Extensions
+{
+    public static class InputBindingsExtension
+    {
+        public static readonly DependencyProperty UpdatePropertySourceOnEnterProperty = DependencyProperty.RegisterAttached(
+                "UpdatePropertySourceOnEnter",
+                typeof(DependencyProperty),
+                typeof(Control),
+                new PropertyMetadata());
+
+        public static void SetUpdatePropertySourceOnEnter(DependencyObject dependencyObject, DependencyProperty value)
+        {
+            var control = dependencyObject as Control;
+            if (control != null)
+            {
+                control.KeyDown -= OnKeyDown;
+                control.KeyDown += OnKeyDown;
+            }
+
+            dependencyObject.SetValue(UpdatePropertySourceOnEnterProperty, value);
+        }
+
+        public static DependencyProperty GetUpdatePropertySourceOnEnter(DependencyObject dependencyObject)
+        {
+            return (DependencyProperty)dependencyObject.GetValue(UpdatePropertySourceOnEnterProperty);
+        }
+
+        private static void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+
+            var dependencyObject = sender as DependencyObject;
+            if (dependencyObject == null) return;
+            BindingOperations.GetBindingExpression(dependencyObject, GetUpdatePropertySourceOnEnter(dependencyObject))?.UpdateSource();
+        }
+    }
+}
---
+++ Heibroch.Common/ScopeSlimmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heibroch.Common.Wpf
+{
+    public class ScopeSlimmer<T>
+    {
+        public Func<T, string> MatchProperty { get; set; }
+        public IEnumerable<T> CurrentScope { get; set; }
+        public IEnumerable<T> InitialScope { get; set; }
+        
+        public ScopeSlimmer(IEnumerable<T> initialScope, Func<T, string> matchProperty)
+        {
+            MatchProperty = matchProperty;
+            InitialScope = initialScope;
+            CurrentScope = initialScope;
+        }
+
+        public void Search(string query)
+        {
+            if(CurrentScope == null)
+                
+            CurrentScope = CurrentScope.Where(x => MatchProperty(x).Contains(query));
+        }
+    }
+}
