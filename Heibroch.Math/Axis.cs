2bf8bbdee1dc982fa732a3d98de517c5b57c5ffd
Dwayne Heibroch Petersen <dwayne@live.dk>
2016-06-08 08:59:46 +0200

    asdf

--- Heibroch.Math/Axis.cs
+++ Heibroch.Math/Axis.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Maths = System.Math;
 
 namespace Heibroch.Math
 {
     public static class Axis
     {
-        private static List<double> CreateValuesAtIntervals(double minimum,
-                                                            double maximum,
-                                                            double valueSpacing = 1000,
-                                                            bool includeZero = true)
+        public static List<double> CreateValuesAtIntervals(double minimum,
+                                                           double maximum,
+                                                           double valueSpacing = 1000,
+                                                           bool includeZero = true)
         {
             //For each thousand, then create a tick
             var tickValues = new List<double>();
@@ -19,7 +18,7 @@ namespace Heibroch.Math
                 if (!includeZero && Maths.Abs(i) < double.Epsilon) continue;
                 tickValues.Add(Maths.Round(i / valueSpacing, 0) * valueSpacing);
             }
-            
+
             return tickValues;
         }
     }
