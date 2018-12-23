94f591d1ab8f0c0b5db031febe971981c9c8ee59
Dwayne Heibroch Petersen <Dwayne@live.dk>
2018-12-23 23:14:00 +0100

    Added event bus

---
+++ Heibroch.Common/EventBus.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Heibroch.Common
+{
+    public interface IEventBus
+    {
+        void Subscribe<T>(Action<T> action);
+
+        void Unsubscribe<T>(Action<T> action);
+
+        void Publish<T>(T value);
+    }
+
+    public class EventBus : IEventBus
+    {
+        public Dictionary<Type, List<object>> subscribers;
+        
+        public EventBus()
+        {
+            subscribers = new Dictionary<Type, List<object>>();
+        }
+
+        public void Publish<T>(T value)
+        {
+            var type = typeof(T);
+
+            if (!subscribers.ContainsKey(type)) return;
+
+            foreach(var actionOjbect in subscribers[type])
+            {
+                var action = (Action<T>)actionOjbect;
+                action(value);
+            }
+        }
+
+        public void Subscribe<T>(Action<T> action)
+        {
+            var type = typeof(T);
+
+            if (!subscribers.ContainsKey(type))
+                subscribers.Add(type, new List<object>());
+
+            var actionList = subscribers[type];
+            if (actionList.Contains(action)) return;
+
+            actionList.Add(action);
+        }
+
+        public void Unsubscribe<T>(Action<T> action)
+        {
+            var type = typeof(T);
+
+            if (!subscribers.ContainsKey(type)) return;
+
+            var actionList = subscribers[type];
+            if (!actionList.Contains(action)) return;
+
+            actionList.Remove(action);
+        }
+
+        private void Try<T>(Action<T> action, T value)
+        {
+            try
+            {
+                action(value);
+            }
+            catch
+            {
+                //Suppress
+            }
+        }
+    }
+}
