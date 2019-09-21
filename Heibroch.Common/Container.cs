940ba1ccefa32e673842d0e6afefda4d88a5f399
Dwayne Heibroch Petersen <Dwayne@live.dk>
2019-09-21 19:35:14 +0200

    Added uic container and removed unnecessary files

---
+++ Heibroch.Common/Container.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Heibroch.Common
+{
+    public interface IContainer
+    {
+        void Register<T>(T obj);
+        T Resolve<T>();
+    }
+
+    public class Container : IContainer
+    {
+        public static IContainer Current = new Container();
+
+        readonly Dictionary<Type, object> objects = new Dictionary<Type, object>();
+
+        public void Register<T>(T obj)
+        {
+            objects[typeof(T)] = obj;
+        }
+
+        public T Resolve<T>()
+        {
+            if (!objects.ContainsKey(typeof(T)))
+                objects[typeof(T)] = typeof(T).GetConstructors()[0].Invoke(new object[0]);
+
+            return (T)objects[typeof(T)];
+        }
+    }
+}
