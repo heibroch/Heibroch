bfbe7871d6c40b6636035f005f0b76a67be30c4d
Dwayne Heibroch Petersen <Dwayne@live.dk>
2020-03-10 16:28:24 +0100

    Added interactivity

---
+++ Heibroch.WindowsInteractivity/ClipboardInterop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Heibroch.WindowsInteractivity
+{
+    public sealed class ClipboardInterop
+    {
+        public static event EventHandler ClipboardUpdate;
+        private static ClipboardInteropInternal clipboardInteropInternal = new ClipboardInteropInternal();
+        private static void OnClipboardUpdate(EventArgs e) => ClipboardUpdate?.Invoke(null, e);
+
+        private class ClipboardInteropInternal : Form
+        {
+            public ClipboardInteropInternal()
+            {
+                SetParent(Handle, HWND_MESSAGE);
+                AddClipboardFormatListener(Handle);
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == WM_CLIPBOARDUPDATE)
+                {
+                    OnClipboardUpdate(null);
+                }
+                base.WndProc(ref m);
+            }
+
+            // See http://msdn.microsoft.com/en-us/library/ms649021%28v=vs.85%29.aspx
+            public const int WM_CLIPBOARDUPDATE = 0x031D;
+            public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+            // See http://msdn.microsoft.com/en-us/library/ms632599%28VS.85%29.aspx#message_only
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+            // See http://msdn.microsoft.com/en-us/library/ms633541%28v=vs.85%29.aspx
+            // See http://msdn.microsoft.com/en-us/library/ms649033%28VS.85%29.aspx
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        }
+    }
+}
---
+++ Heibroch.WindowsInteractivity/KeyboardInterop.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Heibroch.WindowsInteractivity
+{
+    public class KeyPressed
+    {
+        public int Key { get; set; }
+
+        public bool ProcessKey { get; set; } = true;
+    }
+
+    public sealed class KeyboardInterop
+    {
+        public static event EventHandler KeyPressed;
+        private static KeyboardInteropInternal keyboardInteropInternal = new KeyboardInteropInternal();
+        private static void OnClipboardUpdate(KeyPressed keyPressed, EventArgs e) => KeyPressed?.Invoke(keyPressed, e);
+
+        private class KeyboardInteropInternal
+        {
+            private const int WH_KEYBOARD_LL = 13;
+            private const int WM_KEYDOWN = 0x0100;
+            private static LowLevelKeyboardProc proc = HookCallback;
+            private static IntPtr hookId = IntPtr.Zero;
+            
+            public KeyboardInteropInternal()
+            {
+                hookId = SetHook(proc);
+            }
+
+            ~KeyboardInteropInternal()
+            {
+                UnhookWindowsHookEx(hookId);
+            }
+
+            private static IntPtr SetHook(LowLevelKeyboardProc proc)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                {
+                    using (ProcessModule curModule = curProcess.MainModule)
+                    {
+                        return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                    }
+                }
+            }
+
+            private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+            private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+            {
+                if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    var globalKeyPressed = new KeyPressed() { Key = Marshal.ReadInt32(lParam) };
+
+                    OnClipboardUpdate(globalKeyPressed, new EventArgs());
+
+                    if (!globalKeyPressed.ProcessKey) return new IntPtr(1);
+                }
+
+                return CallNextHookEx(hookId, nCode, wParam, lParam);
+            }
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+    } 
+}
